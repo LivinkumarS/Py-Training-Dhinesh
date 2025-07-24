004409276726c38236063ac5ac0e0777f0c9af7f
LivinkumarS <001liv100@ggmail.com>
2025-07-24 14:13:16 +0530

    Variables, Datatypes and Type casting

---
+++ VariablesDataTypesTypecasting.py
@@ -0,0 +1,41 @@
+'''Print=90
+
+Num_1=1
+print(90)
+
+a=10
+a=12
+print(a)'''
+
+
+# Datatype
+# Integer
+
+# a=10
+# print(a+10)
+
+# Float 
+
+# a=10.23
+# b=a+10.77
+
+# print(b)
+
+# String
+
+# a='Hello'
+# b="Hi"
+# c="34"
+
+name="Vijay"
+age=55
+
+print(f"His name is {name} and his age is {str(age)}!")
+
+# Boolean
+# a=False
+# print(a)
+
+
+# a=0
+# print(type(a))

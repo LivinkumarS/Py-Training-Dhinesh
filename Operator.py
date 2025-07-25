532f4acac2e00c499b57025a711054ff83f3830d
LivinkumarS <001liv100@ggmail.com>
2025-07-25 16:00:27 +0530

    Operators in Python

---
+++ Operator.py
@@ -0,0 +1,47 @@
+# Arithmetic Operator
+# a=4
+# b=5
+
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)
+# print(a//b)
+# print(a%b)
+# print(a**b)
+
+# print(2+3-5*5**2/5)
+
+# Comparison Operator 
+
+# a=10
+# b=20
+
+# print(a!=b)
+
+# Logical Operator
+# a=False
+# b=True
+
+# print(a and b)
+# print(a or b)
+# print(not a)
+
+# Assignment Operator
+
+# a=5
+# b=55
+
+# a**=2
+
+# print(a)
+
+# Bitwise Operator
+
+a=5
+b=4
+
+# print(a & b)
+# print(a | b)
+# print(a ^ b)
+# print(~8)

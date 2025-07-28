1955759a62038901ae7b5f94db9c54aa2b537bd7
LivinkumarS <001liv100@ggmail.com>
2025-07-28 16:01:36 +0530

    Control Flow

---
+++ ControlFlow.py
@@ -0,0 +1,30 @@
+# control Flow
+
+a=10
+
+# if (a==10 and a>10):
+#     print("If Block")
+# elif a<=10:
+#     print("Elif 1 Block")
+# elif a>=9:
+#     print("Elif 2 Block!")
+# else:
+#     print("Else Block")
+    
+# if True:
+#     if False:
+#         print("Inside Nested If!")
+
+# input from user--> age
+# age>=18---> "Can Vote"
+# age<18---> "Cannot Vote"
+
+# age=int(input("Enter Your Age: "))
+
+# if age>=18:
+#     print("Can Vote")
+# else:
+#     print("Cannot Vote")
+
+a= 20 if 20>=30 else 40
+print(a)

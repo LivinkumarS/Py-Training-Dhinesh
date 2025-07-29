c5b6217c6ec0750dcb0ea7d01d7e76df7a1b9a7a
LivinkumarS <001liv100@ggmail.com>
2025-07-29 16:00:57 +0530

    Loops --> For and While

---
+++ Loops.py
@@ -0,0 +1,31 @@
+# i=0
+
+# while i<10:
+#     print("Step1")
+#     print("Step2")
+#     print("Step3")
+#     print()
+#     print("------------------------")
+#     print()
+    
+#     i+=1
+
+# i=0,1,2,3,4,5,6,7,8,9,10 (0-9)
+
+
+# str="Hello World"
+# print(str[0])
+
+# for num in range(3,10,2): #[3,5,7,9]
+#     print(num)
+
+
+# num = int(input ("Enter the Number"))
+
+# for a in range(1,11):
+#     print(f"{a} X {num} ={a*num}")
+
+for i in range(51):
+    if i==15:
+        continue
+    print(i)

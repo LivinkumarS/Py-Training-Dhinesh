9a03d70aca5152c91899326d2f4b5a93bf5248be
LivinkumarS <001liv100@ggmail.com>
2025-08-07 11:54:36 +0530

    Strings in Python

---
+++ String.py
@@ -0,0 +1,117 @@
+sentence1='I would love to code!'
+
+para='''For other people with the same surname, see Tendulkar (surname).
+Sachin Tendulkar
+
+Tendulkar in 2017
+Personal information
+Full name	
+Sachin Ramesh Tendulkar
+Born	24 April 1973 (age 52)
+Bombay, India
+Nickname	
+Little MasterMaster Blaster
+[1][2]
+Height	165 cm (5 ft 5 in)
+Batting	Right-handed
+Bowling	
+Right-arm leg break
+Right-arm off break
+Role	Top order Batter
+Relations	
+Ramesh Tendulkar (father)
+Arjun Tendulkar (son)
+Website	sachintendulkar.com
+International information
+National side	
+India (1989–2013)
+Test debut (cap 187)	15 November 1989 v Pakistan
+Last Test	14 November 2013 v West Indies
+ODI debut (cap 74)	18 December 1989 v Pakistan
+Last ODI	18 March 2012 v Pakistan
+ODI shirt no.	10 (formerly 99, 33)
+Only T20I (cap 11)	1 December 2006 v South Africa
+T20I shirt no.	10
+Domestic team information
+Years	Team
+1988–2013	Mumbai
+1992	Yorkshire
+1994	East Bengal[3]
+2008–2013	Mumbai Indians (squad no. 10)
+Career statistics
+Competition	Test	ODI	FC	LA
+Matches	200	463	310	551
+Runs scored	15,921	18,426	25,396	21,999
+Batting average	53.78	44.83	57.84	45.54
+100s/50s	51/68	49/96	81/116	60/114
+Top score	248*	200*	248*	200*
+Balls bowled	4,240	8,054	7,605	10,230
+Wickets	46	154	71	201
+Bowling average	54.17	44.48	61.74	42.17
+5 wickets in innings	0	2	0	2
+10 wickets in match	0	0	0	0
+Best bowling	3/10	5/32	3/10	5/32
+Catches/stumpings	115/–	140/–	186/–	175/–
+Medal record
+Source: ESPNcricinfo, 15 November 2013
+Member of Parliament, Rajya Sabha
+In office
+27 April 2012 – 26 April 2018
+Constituency	Nominated
+Personal details
+Awards	
+Bharat Ratna
+Padma Vibhushan
+Padma Shri
+Maharashtra Bhushan
+Khel Ratna Award
+Arjuna Award
+Honorary Member of the Order of Australia
+Signature	
+Military service
+Allegiance	 India
+Branch/service	 Indian Air Force
+Years of service	2010–present
+Rank	 Honorary Group Captain[4]
+	
+This article is part of
+a series about
+Sachin Tendulkar
+Indian International Cricketer
+International centuriesCareer achievements
+Achievements
+Eponyms
+Stand at Wankhede StadiumAnderson–Tendulkar TrophyMarengo sachintendulkar
+In Media
+Ferrari Ki SawaariSachin! Tendulkar AllaSachin: A Billion DreamsSachinSachin: The Ultimate WinnerTendulkar Out
+Books
+Playing It My Way (Autobiography)
+People
+Ramesh Tendulkar FatherArjun Tendulkar SonRamakant Achrekar CoachSudhir Kumar Chaudhary Fan
+Gallery: Picture, Sound, Video
+vte
+Sachin Ramesh Tendulkar (/ˌsʌtʃɪn tɛnˈduːlkər/ ⓘ; Marathi: [sətɕin t̪eɳɖulkəɾ]; born 24 April 1973) is an Indian former international cricketer who captained the Indian national team. He is widely regarded as one of the greatest cricketers of all time,[5] and is the holder of several world records, including being the all-time highest run-scorer in both ODI and Test cricket,[6] receiving the most player of the match awards in international cricket,[7] and being the only batsman to score 100 international centuries.[8] Tendulkar was a Member of Parliament, Rajya Sabha by presidential nomination from 2012 to 2018.'''
+
+# print(para)
+# sentence2="I would love to workout!"
+
+# print(sentence2.upper())
+# print(sentence2.lower())
+# print(sentence2.strip())
+# print(sentence2.replace("would","might"))
+# print(sentence2)
+# print(sentence2.find("livin"))
+# print(sentence2.count('y'))
+# print(sentence2.startswith("Iw"))
+# print(sentence2.endswith("out"))
+
+# lis=["apple","orange","pine apple","banana","grapes"]
+
+# print(','.join(lis))
+
+
+
+
+# lis=[1,2,3,4,5]
+# lis[3]=100
+# print(lis)

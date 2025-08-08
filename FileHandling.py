dcb0ce74e7c7689902538ab28125e0ffd2fa0727
LivinkumarS <001liv100@ggmail.com>
2025-08-08 11:35:24 +0530

    File handling

---
+++ FileHandling.py
@@ -0,0 +1,22 @@
+# Read
+
+# file=open('filehandling.txt','r')
+# # print(file)
+# # content=file.read()
+# firstline=file.readline()
+# firstline=file.readline()
+# firstline=file.readline()
+# firstline=file.readline()
+# print(firstline)
+# file.close()
+
+
+# Write
+# file= open('filehandling.txt','w')
+# file.write('The replaced text\nwith thses contents')
+# file.close()
+
+# Append
+# file=open('filehandling.txt','a')
+# file.write('\nThe appended text\nfrom python..!')
+# file.close()
